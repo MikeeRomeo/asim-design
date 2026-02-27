5c7bb8d858fa311381b9b6ced8761438693807c4
Mike Romeo <mikeeromeo@gmail.com>
2026-02-27 20:44:20 +0100

    feat: add tailwind

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,11 @@
 <template>
   <div>
     <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <div class="font-base">
+      <h1 class="text-3xl font-semibold underline font-unbounded text-testing">
+        dit is een test tekst
+      </h1>
+    </div>
+    <!-- <NuxtWelcome /> -->
   </div>
 </template>
