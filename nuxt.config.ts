83bf81e8dc6a4fa6e4b36c565321bba5778bebef
Mike Romeo <mikeeromeo@gmail.com>
2026-03-17 21:26:02 +0100

    fixing things

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,10 +2,13 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineNuxtConfig({
   app: {
-    baseURL: process.env.NUXT_APP_BASE_URL || '/',
+    baseURL: '/asim-design/',
   },
   nitro: {
     preset: 'github_pages',
+    prerender: {
+      failOnError: false,
+    },
   },
   compatibilityDate: '2025-07-15',
   modules: ['@vueuse/nuxt', 'vue3-carousel-nuxt'],
