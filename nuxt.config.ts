a0974d4edcbb39cd9c29f6d3d008281c7c663961
Mike Romeo <mikeeromeo@gmail.com>
2026-03-17 20:35:42 +0100

    added env to config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineNuxtConfig({
+  baseURL: process.env.NUXT_APP_BASE_URL || '/',
   compatibilityDate: '2025-07-15',
   modules: ['@vueuse/nuxt', 'vue3-carousel-nuxt'],
   devtools: { enabled: true },
